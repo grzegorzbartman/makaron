f8d408ae69cff9fec85b5a54d9e9d1c71856ff01
Grzegorz Bartman <grzegorz.bartman@openbit.pl>
2025-11-28 18:17:14 +0100

    Fix battery icons with Material Design Nerd Fonts

--- configs/sketchybar/plugins/battery.sh
+++ configs/sketchybar/plugins/battery.sh
@@ -23,19 +23,19 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="󰁹"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="󰂀"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="󰁾"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="󰁻"
   ;;
-  *) ICON=""
+  *) ICON="󰂃"
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+  ICON="󰂄"
 fi
 
 # Determine colors - use focused border color for low battery warning
